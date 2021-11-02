1c04217ea3cd7fb11eca8f7a59efa25959c43e4d
encodedBicoding <dominicisioma000@gmail.com>
2021-11-02 15:35:36 +0100

    rebuild new design

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,18 @@
 </template>
 
 <style>
+body {
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  width: 100%;
+  position: relative;
 }
 * {
   box-sizing: content-box;
